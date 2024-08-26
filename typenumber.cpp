34424986d6e14fd1c59cbec7c471239fa965e8b3
yousef_fahem <yousef.fahem11@gmail.com>
2024-08-26 12:10:08 +0300

    Create typenumber.cpp

---
+++ typenumber.cpp
@@ -0,0 +1,15 @@
+//takes a number from user and checks if this number is positive or negative 
+
+float num;
+ cout << "enter the number\n";
+ cin >> num;
+ if (num > 0) {
+ cout << "the number is postive\n";
+ }
+ else if(num==0){
+ cout << "the number equal zero\n";
+ }
+ else {
+ cout << "the number is negative\n";
+ }
+ 
